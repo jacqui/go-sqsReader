8f556eb114e798b39766cf17a9986a2451a953e2
Nik Hanselmann <nikhan@gmail.com>
2014-01-31 15:30:41 +0000

    remove old stuff

--- reader.go
+++ reader.go
@@ -24,9 +24,6 @@ type Reader struct {
     signatureVersion string
     waitTime         string
     maxMsgs          string
-    pollChan         chan bool                   // triggers a poll
-    msgChan          chan *sqsMessage            // messages to be handled
-    delChan          chan []string               // receipt handles to be deleted from queue
     QuitChan         chan bool                   // stops the reader
     OutChan          chan []byte                 // output channel for the client
 }
@@ -51,9 +48,6 @@ func NewReader(sqsEndpoint, accessKey, accessSecret string, outChan chan []byte)
         signatureVersion: AWSSignatureVersion,
         waitTime:         "0",  // in seconds
         maxMsgs:          "10", // in messages
-        pollChan:         make(chan bool),
-        msgChan:          make(chan *sqsMessage),
-        delChan:          make(chan []string),
         QuitChan:         make(chan bool),
         OutChan:          outChan,
     }
@@ -72,64 +66,68 @@ func (r *Reader) Start() {
     queryurl := r.sqsEndpoint + query.Encode()
 
     for{
-        var m sqsMessage
-        var m1 map[string]interface{}
+        select {
+        case <-r.QuitChan:
+            return
+        default:
+            var m sqsMessage
+            var m1 map[string]interface{}
 
-        resp, err := r.client.Get(queryurl)
+            resp, err := r.client.Get(queryurl)
 
-        if err != nil {
-            continue
-        }
-        
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            continue
-        }
-
-        resp.Body.Close()
-
-        err = xml.Unmarshal(body, &m)
-        if err != nil {
-            continue
-        }
-
-        for _, body := range m.Body {
-            err = json.Unmarshal([]byte(body), &m1)
             if err != nil {
                 continue
             }
-            msgString, ok := m1["Message"].(string)
-            if !ok {
+            
+            body, err := ioutil.ReadAll(resp.Body)
+            if err != nil {
                 continue
             }
-            msgs := strings.Split(msgString, "\n")
-            for _, msg := range msgs {
-                if len(msg) == 0 {
+
+            resp.Body.Close()
+
+            err = xml.Unmarshal(body, &m)
+            if err != nil {
+                continue
+            }
+
+            for _, body := range m.Body {
+                err = json.Unmarshal([]byte(body), &m1)
+                if err != nil {
                     continue
                 }
-                r.OutChan <- []byte(msg)
+                msgString, ok := m1["Message"].(string)
+                if !ok {
+                    continue
+                }
+                msgs := strings.Split(msgString, "\n")
+                for _, msg := range msgs {
+                    if len(msg) == 0 {
+                        continue
+                    }
+                    r.OutChan <- []byte(msg)
+                }
             }
-        }
 
-        delquery := url.Values{}
-        delquery.Set("Action", "DeleteMessageBatch")
-        delquery.Set("Version", r.version)
-        delquery.Set("SignatureVersion", r.signatureVersion)
-        for i, r := range m.ReceiptHandle {
-            id  := fmt.Sprintf("DeleteMessageBatchRequestEntry.%d.Id", (i + 1))
-            receipt := fmt.Sprintf("DeleteMessageBatchRequestEntry.%d.ReceiptHandle", (i + 1))
-            delquery.Add(id, fmt.Sprintf("msg%d",(i+1)))
-            delquery.Add(receipt, r)
-        }
-        delurl := r.sqsEndpoint + delquery.Encode()
+            delquery := url.Values{}
+            delquery.Set("Action", "DeleteMessageBatch")
+            delquery.Set("Version", r.version)
+            delquery.Set("SignatureVersion", r.signatureVersion)
+            for i, r := range m.ReceiptHandle {
+                id  := fmt.Sprintf("DeleteMessageBatchRequestEntry.%d.Id", (i + 1))
+                receipt := fmt.Sprintf("DeleteMessageBatchRequestEntry.%d.ReceiptHandle", (i + 1))
+                delquery.Add(id, fmt.Sprintf("msg%d",(i+1)))
+                delquery.Add(receipt, r)
+            }
+            delurl := r.sqsEndpoint + delquery.Encode()
 
-        resp, err = r.client.Get(delurl)
-        if err != nil {
-            continue
+            resp, err = r.client.Get(delurl)
+            if err != nil {
+                continue
+            }
+            
+            resp.Body.Close()
         }
-        
-        resp.Body.Close()
-
     }
 }
 
